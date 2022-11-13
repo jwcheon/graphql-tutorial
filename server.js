09f23a951951f2f757b7c7e2e4426574f8433c8c
Jinwoo Cheon <jinwoocheon@yonsei.ac.kr>
2022-11-14 05:06:11 +0900

    Non Nullable Fields

--- server.js
+++ server.js
@@ -3,26 +3,28 @@ import { ApolloServer, gql } from "apollo-server";
 // - type Query is the mandatory part
 // - Scalar & Non-scalar types
 // - Mutation type: causes mutation in data (= like POST DELETE PUT PATCH in REST API)
+// - Non Nullable Fields: mandatory values. w/o ! it's nullable(ex. Tweet|null)
+//   - So 'tweet' in Query might not have a matching ID in DB to return so it's nullable
 
 const typeDefs = gql`
     type User {
-        id: ID
-        username: String
+        id: ID!
+        username: String!
+        firstName: String
+        lastName: String
     }
     type Tweet {
-        id: ID
-        text: String
-        author: User
+        id: ID!
+        text: String!
+        author: User!
     }
     type Query {
-        allTweets: [Tweet]
-        tweet(id: ID): Tweet
-        allUsers: [User]
-        user(id: ID): User
+        allTweets: [Tweet!]!
+        tweet(id: ID!): Tweet
     }
     type Mutation {
-        postTweet(text: String, userId: ID): Tweet
-        deleteTweet(id: ID): Boolean
+        postTweet(text: String!, userId: ID!): Tweet!
+        deleteTweet(id: ID!): Boolean!
     }
 `;
 
