fbd7ad5d54b8ba632dbcb9836e34e0ef8589cc5a
Jinwoo Cheon <jinwoocheon@yonsei.ac.kr>
2022-11-14 04:26:00 +0900

    type Query

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const server = new ApolloServer({})
+// type Query is the mandatory part
+// below is same as /text and /hello in REST API
+const typeDefs = gql`
+    type Query {
+        text: String
+        hello: String
+    }
+`;
+
+const server = new ApolloServer({typeDefs});
+
 server.listen().then(({url}) => {
-    console.log(`Running on ${url}`)
+    console.log(`Running on ${url}`);
 });
