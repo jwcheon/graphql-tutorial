882351b3fa23d2502b48db3b4c10ab3aa47a8be3
Jinwoo Cheon <jinwoocheon@yonsei.ac.kr>
2022-11-15 00:16:09 +0900

    Working w/ Resolvers

--- server.js
+++ server.js
@@ -5,18 +5,30 @@ import { ApolloServer, gql } from "apollo-server";
 // - Mutation type: causes mutation in data (= like POST DELETE PUT PATCH in REST API)
 // - Non Nullable Fields: mandatory values. w/o ! it's nullable(ex. Tweet|null)
 //   - So 'tweet' in Query might not have a matching ID in DB to return so it's nullable
+// - Resolver: to handle the query. Sends in (root, args)
+
+const tweets = [
+    {
+        id: "1",
+        text: "First one",
+    },
+    {
+        id: "2",
+        text: "Second one",
+    }
+]
 
 const typeDefs = gql`
     type User {
         id: ID!
         username: String!
-        firstName: String
+        firstName: String!
         lastName: String
     }
     type Tweet {
         id: ID!
         text: String!
-        author: User!
+        author: User
     }
     type Query {
         allTweets: [Tweet!]!
@@ -28,7 +40,18 @@ const typeDefs = gql`
     }
 `;
 
-const server = new ApolloServer({typeDefs});
+const resolvers = {
+    Query: {
+        allTweets() {
+            return tweets;
+        },
+        tweet(root, { id }) {
+            return tweets.find(tweet => tweet.id === id);
+        }
+    },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({url}) => {
     console.log(`Running on ${url}`);
